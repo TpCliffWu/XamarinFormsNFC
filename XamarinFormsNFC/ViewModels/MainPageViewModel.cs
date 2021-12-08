01689a88a3ff10e22b37cc57a1dbb7c494ab3141
CliffWu <cliff.wu@thinkpower-info.com>
2021-12-08 09:55:24 +0800

    讀寫

--- XamarinFormsNFC/ViewModels/MainPageViewModel.cs
+++ XamarinFormsNFC/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Plugin.NFC;
+using Prism.AppModel;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
@@ -8,16 +9,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using XamarinFormsNFC.NFCFunction;
 
 namespace XamarinFormsNFC.ViewModels
 {
-    public class MainPageViewModel : ViewModelBase
+    public class MainPageViewModel : ViewModelBase, IPageLifecycleAware
     {
         public DelegateCommand ReadCommand { get; set; }
         public DelegateCommand WritingCommand { get; set; }
+        public DelegateCommand StopCommand { get; set; }
 
+
+        private bool _nfcIsEnabled;
+        public bool NfcIsEnabled
+        {
+            get { return _nfcIsEnabled; }
+            set
+            {
+                SetProperty(ref _nfcIsEnabled, value);
+                NfcIsDisabled = !value;
+            }
+        }
+
+        private bool _nfcIsDisabled;
+        public bool NfcIsDisabled
+        {
+            get { return _nfcIsDisabled; }
+            set { SetProperty(ref _nfcIsDisabled, value); }
+        }
+
+
+
+        bool _makeReadOnly = false;
+        bool _eventsAlreadySubscribed = false;
+        bool _isDeviceiOS = false;
 
         public const string ALERT_TITLE = "NFC";
         public const string MIME_TYPE = "application/com.tp.nfc";
@@ -33,16 +59,73 @@ namespace XamarinFormsNFC.ViewModels
         public MainPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "NFC";
-
+            NfcContent = "20211208";
+            NfcIsEnabled = true;
+            NfcIsDisabled = true;
             ReadCommand = new DelegateCommand(ReadNfcTag);
             WritingCommand = new DelegateCommand(WritingNfc);
+            StopCommand = new DelegateCommand(Stop);
+
         }
 
-        private async void WritingNfc()
+        public void OnAppearing()
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                NFCSubscribe();
+            });
+        }
+
+        public void OnDisappearing()
+        {
+        }
+
+
+        private async void Stop()
         {
             try
             {
-                CrossNFC.Current.StartPublishing(true);
+                CrossNFC.Current.StopListening();
+
+            }
+            catch (Exception ex)
+            {
+                await ShowAlert(ex.Message);
+            }
+        }
+
+
+        public override void OnResume()
+        {
+            base.OnResume();
+        }
+
+
+        private async void NFCSubscribe()
+        {
+            if (CrossNFC.IsSupported)
+            {
+                if (!CrossNFC.Current.IsAvailable)
+                    await ShowAlert("NFC is not available");
+
+                NfcIsEnabled = CrossNFC.Current.IsEnabled;
+                if (!NfcIsEnabled)
+                    await ShowAlert("NFC is disabled");
+
+                if (Device.RuntimePlatform == Device.iOS)
+                    _isDeviceiOS = true;
+
+                SubscribeEvents();
+
+                await StartListeningIfNotiOS();
+            }
+        }
+
+        private async void ReadNfcTag()
+        {
+            try
+            {
+                CrossNFC.Current.StartListening();
             }
             catch (System.Exception ex)
             {
@@ -50,27 +133,89 @@ namespace XamarinFormsNFC.ViewModels
             }
         }
 
+        private async void WritingNfc()
+        {
+            await Publish(NFCNdefTypeFormat.Mime);
+        }
 
-        private async void ReadNfcTag()
+        async Task Publish(NFCNdefTypeFormat? type = null)
+        {
+            await StartListeningIfNotiOS();
+            try
+            {
+                //if (ChkReadOnly.IsChecked)
+                //{
+                //    if (!await DisplayAlert("Warning", "Make a Tag read-only operation is permanent and can't be undone. Are you sure you wish to continue?", "Yes", "No"))
+                //    {
+                //        ChkReadOnly.IsChecked = false;
+                //        return;
+                //    }
+                //    _makeReadOnly = true;
+                //}
+                //else
+                //    _makeReadOnly = false;
+
+                if (type.HasValue) _type = type.Value;
+                CrossNFC.Current.StartPublishing(!type.HasValue);
+            }
+            catch (Exception ex)
+            {
+                await ShowAlert(ex.Message);
+            }
+        }
+
+        async Task StartListeningIfNotiOS()
+        {
+            if (_isDeviceiOS)
+                return;
+            await BeginListening();
+        }
+
+        async Task BeginListening()
+        {
+            try
+            {
+                CrossNFC.Current.StartListening();
+            }
+            catch (Exception ex)
+            {
+                await ShowAlert(ex.Message);
+            }
+        }
+
+        public async Task<bool> CheckPermission()
         {
             if (CrossNFC.IsSupported)
             {
                 //檢查 CrossNFC.Current.IsAvailable 以確認 NFC 是否可用。
                 if (!CrossNFC.Current.IsAvailable)
+                {
                     await ShowAlert("NFC is not available");
+                    return false;
+                }
+
                 //檢查 CrossNFC.Current.IsEnabled 以確認是否啟用了 NFC。
                 if (!CrossNFC.Current.IsEnabled)
+                {
                     await ShowAlert("NFC is disabled");
+                    return false;
+                }
+
                 //訂閱NFC事件
                 SubscribeEvents();
 
                 if (Device.RuntimePlatform != Device.iOS)
                 {
-                    // Start NFC tag listening manually
+                    // Start NFC tag listening manua\ly
                     CrossNFC.Current.StartListening();
                 }
+                return true;
             }
+            return false;
         }
+
+
+
 
         void SubscribeEvents()
         {
@@ -84,6 +229,17 @@ namespace XamarinFormsNFC.ViewModels
             if (Device.RuntimePlatform == Device.iOS)
                 CrossNFC.Current.OniOSReadingSessionCancelled += Current_OniOSReadingSessionCancelled;
         }
+
+        void UnsubscribeEvents()
+        {
+            CrossNFC.Current.OnMessageReceived -= Current_OnMessageReceived;
+            CrossNFC.Current.OnMessagePublished -= Current_OnMessagePublished;
+            CrossNFC.Current.OnTagDiscovered -= Current_OnTagDiscovered;
+
+            if (Device.RuntimePlatform == Device.iOS)
+                CrossNFC.Current.OniOSReadingSessionCancelled -= Current_OniOSReadingSessionCancelled;
+        }
+
 
         async void Current_OniOSReadingSessionCancelled(object sender, EventArgs e) => await ShowAlert("User has cancelled NFC reading session");
 
@@ -149,14 +305,15 @@ namespace XamarinFormsNFC.ViewModels
                         {
                             TypeFormat = NFCNdefTypeFormat.WellKnown,
                             MimeType = MIME_TYPE,
-                            Payload = NFCUtils.EncodeToByteArray("This is a text message!")
+                            Payload = NFCUtils.EncodeToByteArray("Plugin.NFC is awesome!"),
+                            LanguageCode = "en"
                         };
                         break;
                     case NFCNdefTypeFormat.Uri:
                         record = new NFCNdefRecord
                         {
                             TypeFormat = NFCNdefTypeFormat.Uri,
-                            Payload = NFCUtils.EncodeToByteArray("https://google.fr")
+                            Payload = NFCUtils.EncodeToByteArray("https://github.com/franckbour/Plugin.NFC")
                         };
                         break;
                     case NFCNdefTypeFormat.Mime:
@@ -164,7 +321,7 @@ namespace XamarinFormsNFC.ViewModels
                         {
                             TypeFormat = NFCNdefTypeFormat.Mime,
                             MimeType = MIME_TYPE,
-                            Payload = NFCUtils.EncodeToByteArray("This is a custom record!")
+                            Payload = NFCUtils.EncodeToByteArray($"{NfcContent}")
                         };
                         break;
                     default:
--- XamarinFormsNFC/ViewModels/ViewModelBase.cs
+++ XamarinFormsNFC/ViewModels/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using Prism.AppModel;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using Prism.Services;
@@ -10,7 +11,7 @@ using Unity;
 
 namespace XamarinFormsNFC.ViewModels
 {
-    public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IDestructible
+    public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IDestructible, IApplicationLifecycleAware
     {
         protected INavigationService NavigationService { get; private set; }
 
@@ -53,6 +54,16 @@ namespace XamarinFormsNFC.ViewModels
         public virtual void Destroy()
         {
 
+        }
+
+        public virtual void OnResume()
+        {
+            //Restore the state of your ViewModel.
+        }
+
+        public virtual void OnSleep()
+        {
+            //Save the state of your ViewModel.
         }
     }
 }
